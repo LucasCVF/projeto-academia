a5852bf52d8aa9eee56084e784f0f429939497bc
LucasCVF <lucasferreiracf14@gmail.com>
2025-03-18 22:19:02 -0300

    Personalizando home, finalizando 'unidades' e iniciando 'Sobre nós'

--- wp-content/themes/academia/page.php
+++ wp-content/themes/academia/page.php
@@ -98,29 +98,31 @@ get_header();
 					</div>
 				</div>
 			</section>
-			
-			<hr style="height: 50px;" class="edit-hr">
 		</article>
 
-		<article id="planos">
+		<article id="planos" class="background-article">
 			<hr style="height: 50px;" class="edit-hr">
-			<h1 class="text-center mt-5 mb-5 h1">Fique por dentro dos nossos planos!</h1>
+			<h1 class="text-center m-5 color-h1">Fique por dentro dos nossos planos!</h1>
 			<section class="card-content-plan">
 				<div class="card card-edit-plan">
 					<div class="card-body row">
 						<div class="col-5">	
-							<h1 class="mb-3">Diario</h1>
+							<h1>Diario</h1>
+							<div class="mb-3" style="background: yellow; height:5px;"> </div>
 							<p class="card-text">Ideal para quem quer treinar sem compromisso! Acesse todas as nossas instalações e aproveite um dia de treino completo.</p>
-							<h1 class="mb-3">Mensal</h1>
+							<h1>Mensal</h1>
+							<div class="mb-3" style="background: yellow; height:5px;"> </div>
 							<p class="card-text">Treine sem limites durante 30 dias! Tenha acesso a todas as modalidades e equipamentos, além de acompanhamento personalizado.</p>
 						</div>
 						<div class="col-2">
 							<i class="fa-solid fa-money-check-dollar fa-10x m-5"></i>
 						</div>
 						<div class="col-5">
-							<h1 class="mb-3">Semestral</h1>
+							<h1>Semestral</h1>
+							<div class="mb-3" style="background: yellow; height:5px;"> </div>
 							<p class="card-text">Seis meses para transformar sua rotina! Acesso ilimitado a todas as modalidades, suporte especializado e condições especiais para você manter o foco nos seus objetivos.</p>
-							<h1 class="mb-3">Anual</h1>
+							<h1>Anual</h1>
+							<div class="mb-3" style="background: yellow; height:5px;"> </div>
 							<p class="card-text">Compromisso com seus resultados! Treine o ano inteiro com benefícios exclusivos, descontos especiais e suporte contínuo dos nossos profissionais.</p>
 						</div>
 						</div>
@@ -129,16 +131,16 @@ get_header();
 			</section>
 		</article>
 
-		<article id="unidade">
+		<article id="unidade" class="background-article">
 			<hr style="height: 50px;" class="edit-hr">
-			<h1 class="text-center mt-5 mb-5 h1">Conheça Nossas Unidades!</h1>
+			<h1 class="text-center m-5 color-h1">Conheça Nossas Unidades!</h1>
 			<section class="card-content">
 				<div class="map-container mt-4 text-center row">
 					<div class="col-4">
-						<div class="card align-items-center">
+						<div class="card align-items-center card-uni">
 							<iframe 
 								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31757.97193165947!2d-35.244411894599615!3d-5.749588730885146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b3aa41b5590f17%3A0xf09aba16ea76a173!2sClubFit!5e0!3m2!1spt-BR!2sbr!4v1742258211447!5m2!1spt-BR!2sbr" 
-								width="500" 
+								width="575" 
 								height="400" 
 								style="border:0;" 
 								allowfullscreen="" 
@@ -147,16 +149,15 @@ get_header();
 							</iframe>
 							<div class="card-body">
 								<h3>Unidade Zona norte</h3>
-								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-								<a href="#" class="btn btn-primary">Venha conhecer!</a>
+								<a href="#" class="btn btn-uni">Venha conhecer!</a>
 							</div>
 						</div>	
 					</div>
 					<div class="col-4">
-						<div class="card align-items-center">
+						<div class="card align-items-center card-uni">
 							<iframe 
 								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d44911.17588357399!2d-35.24524757199212!3d-5.767031060089852!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b3070500484c03%3A0xa2796f1cc2b95bc6!2sClubfit%20Redinha!5e0!3m2!1spt-BR!2sbr!4v1742258533521!5m2!1spt-BR!2sbr" 
-								width="500" 
+								width="575" 
 								height="400" 
 								style="border:0;" 
 								allowfullscreen="" 
@@ -164,17 +165,16 @@ get_header();
 								referrerpolicy="no-referrer-when-downgrade">
 							</iframe>
 							<div class="card-body">
-							<h3>Unidade Redinha</h3>
-								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-								<a href="#" class="btn btn-primary">Venha conhecer!</a>
+								<h3>Unidade Redinha</h3>
+								<a href="#" class="btn btn-uni">Venha conhecer!</a>
 							</div>
 						</div>	
 					</div>
-					<div class="col-4">
-						<div class="card align-items-center">
+					<div class="col-4 mb-5">
+						<div class="card align-items-center card-uni">
 							<iframe 
 								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63502.40057322635!2d-35.235962618127836!3d-5.869674699999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b2ff000f48aebf%3A0x13c7d385651f6af2!2sClubFiT%20Academia%20Ponta%20Negra!5e0!3m2!1spt-BR!2sbr!4v1742258582249!5m2!1spt-BR!2sbr" 
-								width="500" 
+								width="575" 
 								height="400" 
 								style="border:0;" 
 								allowfullscreen="" 
@@ -183,9 +183,45 @@ get_header();
 							</iframe>
 							<div class="card-body">
 								<h3>Unidade Ponta Negra</h3>
-								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-								<a href="#" class="btn btn-primary">Venha conhecer!</a>
+								<a href="#" class="btn btn-uni">Venha conhecer!</a>
 							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</article>
+
+		<article id="sobre" class="background-article">
+			<hr style="height: 50px;" class="edit-hr">
+			<h1 class="text-center m-5 color-h1">Sobre nós</h1>
+			<section class="card-content-plan">
+				<div class="card card-edit-plan">
+					<div class="card-body row p-5">
+						<div class="col-6">	
+							<h3>Quem Somos</h3>
+							<p class="card-text">Na ClubFit, acreditamos que a atividade física vai muito além da estética: é um estilo de vida! Nossa missão é proporcionar um ambiente acolhedor, com estrutura de ponta e profissionais capacitados para ajudar você a atingir seus objetivos.</p>
+							<p class="card-text">Desde nossa fundação, buscamos transformar vidas através da saúde e bem-estar, oferecendo treinos personalizados, equipamentos modernos e uma comunidade motivadora.</p>
+							<ul>
+								<h3>Nossa Estrutura</h3>
+								<li>Unidades modernas e bem equipadas</li>
+								<li> Equipamentos de última geração</li>
+								<li>Instrutores qualificados para acompanhamento personalizado</li>
+								<li>Diversas modalidades, incluindo musculação, funcional, dança e muito mais</li>
+								<li>Ambiente seguro e higienizado para o seu conforto</li>
+							</ul>
+						</div>
+						<div class="col-6">
+							<h3>Nossa Missão</h3>
+							<p class="card-text">Inspirar e transformar vidas por meio da atividade física, promovendo saúde, bem-estar e qualidade de vida para todas as idades.</p>
+							<h3>Nossa Visão</h3>
+							<p class="card-text">Ser referência em treinamento físico e qualidade de atendimento, proporcionando experiências únicas para cada aluno.</p>
+							<ul>
+								<h3>Nossos valores</h3>
+								<li>Compromisso com a saúde e o bem-estar</li>
+								<li>Atendimento humanizado e acolhedor</li>
+								<li>Inovação e excelência em nossos serviços</li>
+							</ul>
+						</div>
 						</div>
 					</div>
 				</div>
